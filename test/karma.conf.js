8781193c379378b43232581e4ecc63dede9d6ad9
lijulia <juli@vmware.com>
2017-02-13 12:54:16 -0800

    add unit testing
    Signed-off-by: lijulia <juli@vmware.com>

---
+++ test/karma.conf.js
@@ -0,0 +1,96 @@
+// Karma configuration
+
+module.exports = function(config) {
+  'use strict';
+
+  config.set({
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '../',
+
+    // testing framework to use (jasmine/mocha/qunit/...)
+    // as well as any additional frameworks (requirejs/chai/sinon/...)
+    frameworks: [
+      'jasmine',
+      'jasmine-matchers'
+    ],
+
+    // list of files / patterns to load in the browser
+    files: [
+      // bower:js
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-cookies/angular-cookies.js',
+      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-environment/dist/angular-environment.js',
+      'bower_components/angular-filter/dist/angular-filter.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      // endbower
+      'app/config.js',
+      'app/scripts/**/*.js',
+      'test/mock/**/*.js',
+      'test/spec/**/*.js',
+      'app/views/apis/detail.html',
+    ],
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+    // web server port
+    port: 8080,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: [
+      'PhantomJS'
+    ],
+
+    // Which plugins to enable
+    plugins: [
+      'karma-phantomjs-launcher',
+      'karma-jasmine',
+      'karma-jasmine-matchers'
+      //'karma-ng-html2js-preprocessor' 
+    ],
+    
+    /*
+    preprocessors: { 
+   	  'app/views/apis/detail.html': ['ng-html2js'] 
+   	}, 
+
+    ngHtml2JsPreprocessor: { 
+    	stripPrefix: 'app/', 
+    	moduleName: 'apiExplorerApp.templates' 
+    }, */
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true,
+
+    colors: true,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // Uncomment the following lines if you are using grunt's server to run the tests
+    // proxies: {
+    //   '/': 'http://localhost:9000/'
+    // },
+    // URL root prevent conflicts with the site root
+    // urlRoot: '_karma_'
+  });
+};
