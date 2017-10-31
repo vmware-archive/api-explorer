067227c78482382aec3faa6d027f4fd528462c7b
lijulia <juli@vmware.com>
2017-10-31 14:49:26 -0700

    Upgrade to latest clarity version and added missing lib files
    Signed-off-by: lijulia <juli@vmware.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@
  */
 
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, Injector } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
@@ -25,6 +26,7 @@ import { environment } from '../environments/environment';
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         HttpModule,
         AppRoutingModule,
